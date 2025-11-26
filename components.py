54b9bd6e3264ec93d1805505eedce7a359962ee7
Ha Seung Min <tmdalsgk12@naver.com>
2025-11-26 22:07:51 +0900

    implement flagged_count()

--- components.py
+++ components.py
@@ -158,8 +158,7 @@ def reveal(self, col: int, row: int) -> None:
       cell.state.is_flagged = not cell.state.is_flagged
 
     def flagged_count(self) -> int:
-        # TODO: Return current number of flagged cells.
-        pass
+      return sum(1 for c in self.cells if c.state.is_flagged)
 
     def _reveal_all_mines(self) -> None:
         """Reveal all mines; called on game over."""
